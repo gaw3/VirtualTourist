995d2e663df48e9e948cd53d97aea440a7a8fbd6
gaw3 <gwhite2003@verizon.net>
2016-03-29 21:07:02 -0500

    chore:  Remove playground

--- MyPlayground.playground/Contents.swift
+++
@@ -1,7 +0,0 @@
-//: Playground - noun: a place where people can play
-
-import UIKit
-
-var str = "https://farm2.staticflickr.com/1647/25845547720_ac152b7a4f.jpg"
-var components = str.componentsSeparatedByString("/").last
-
